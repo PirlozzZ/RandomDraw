c20e670c4f46227e44bad40b73fbaaafbc09fac2
Len <Pirlol@163.com>
2017-02-25 14:32:34 +0800

    modified by Len

--- RandomDraw/Form1.cs
+++ RandomDraw/Form1.cs
@@ -16,5 +16,7 @@ namespace RandomDraw
         {
             InitializeComponent();
         }
+
+         
     }
 }
---
+++ RandomDraw/MyConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Windows.Forms;
+
+namespace RandomDraw
+{
+    class MyConfiguration
+    {
+        public static string fileName = System.IO.Path.GetFileName(Application.ExecutablePath);
+        public static bool addSetting(string key, string value)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(fileName);
+            config.AppSettings.Settings.Add(key, value);
+            config.Save();
+            return true;
+        }
+        public static string getSetting(string key)
+        {
+            Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(fileName);
+            string value = config.AppSettings.Settings[key].Value;
+            return value;
+        }
+        public static bool updateSeeting(string key, string newValue)
+        {
+            Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(fileName);
+            string value = config.AppSettings.Settings[key].Value = newValue;
+            config.Save();
+            return true;
+        }
+    }
+}
